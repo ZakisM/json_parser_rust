42a6c2e052f8203286b5c379bca96ff465de1f96
zak <8143258+ZakisM@users.noreply.github.com>
2025-01-01 20:59:41 +0000

    WIP

--- lexer/src/ast.rs
+++ lexer/src/ast.rs
@@ -1,3 +1,11 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct JsonItem<'a> {
+    key: &'a str,
+    value: JsonValue<'a>,
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum JsonValue<'a> {
     Null,
@@ -8,10 +16,47 @@ pub enum JsonValue<'a> {
     Object(Vec<JsonItem<'a>>),
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct JsonItem<'a> {
-    key: &'a str,
-    value: JsonValue<'a>,
+fn to_flattened(root: &JsonValue, prefix: Option<String>) -> HashMap<String, String> {
+    let mut res = HashMap::new();
+
+    match root {
+        JsonValue::Object(items) => {
+            for item in items {
+                let mut key = item.key.to_owned();
+                if let Some(prefix) = &prefix {
+                    key.push('.');
+                    key.push_str(prefix);
+                }
+                let value = &item.value;
+
+                match value {
+                    JsonValue::Null => {
+                        res.insert(key, "null".to_owned());
+                    }
+                    JsonValue::Boolean(b) => {
+                        res.insert(key, b.to_string());
+                    }
+                    JsonValue::Number(n) => {
+                        res.insert(key, n.to_string());
+                    }
+                    JsonValue::String(s) => {
+                        res.insert(key, String::from_utf8_lossy(s).into_owned());
+                    }
+                    JsonValue::Array(vec) => todo!(),
+                    JsonValue::Object(vec) => {
+                        let nested = to_flattened(value, Some(key));
+
+                        res.extend(nested);
+
+                        return res;
+                    }
+                };
+            }
+        }
+        _ => panic!("must be called with a JsonValue::Object"),
+    };
+
+    res
 }
 
 #[cfg(test)]
@@ -19,7 +64,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn walk() {
+    fn flattened() {
         let root = JsonValue::Object(vec![
             JsonItem {
                 key: "string",
@@ -37,17 +82,29 @@ mod tests {
                 key: "null",
                 value: JsonValue::Null,
             },
+            // JsonItem {
+            //     key: "array",
+            //     value: JsonValue::Array(vec![
+            //         JsonValue::Number(1),
+            //         JsonValue::Number(2),
+            //         JsonValue::Number(3),
+            //         JsonValue::Number(4),
+            //         JsonValue::String(b"five"),
+            //         JsonValue::Boolean(true),
+            //     ]),
+            // },
             JsonItem {
-                key: "array",
-                value: JsonValue::Array(vec![
-                    JsonValue::Number(1),
-                    JsonValue::Number(2),
-                    JsonValue::Number(3),
-                    JsonValue::Number(4),
-                    JsonValue::String(b"five"),
-                    JsonValue::Boolean(true),
-                ]),
+                key: "anotherNestedObject",
+                value: JsonValue::Object(vec![JsonItem {
+                    key: "level1",
+                    value: JsonValue::Object(vec![JsonItem {
+                        key: "key",
+                        value: JsonValue::String(b"value"),
+                    }]),
+                }]),
             },
         ]);
+
+        dbg!(&to_flattened(&root, None));
     }
 }
