14b61f7a97f539f2c57f34455937b4541458728b
zak <8143258+ZakisM@users.noreply.github.com>
2025-03-02 18:08:29 +0000

    2024 edition

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ use wide::WideString;
 mod wide;
 
 // SAFETY: Not safe
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "stdcall" fn get_key(input_str_ptr: *const u16, input_str_len: i32) -> *mut u16 {
-    let mut thing = WideString::from_raw_parts(input_str_ptr, input_str_len);
+    let mut thing = unsafe { WideString::from_raw_parts(input_str_ptr, input_str_len) };
     thing.push(" - rustified!");
 
     thing.to_c_wide()
